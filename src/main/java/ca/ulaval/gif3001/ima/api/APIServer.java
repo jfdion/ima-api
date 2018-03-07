a6e5cb1d3b4430e6abff6a3228517340e956be35
Jean-Francois Dion <jeff.dion@gmail.com>
2018-03-07 17:45:21 -0500

    Test pour les variables d'environnement

--- src/main/java/ca/ulaval/gif3001/ima/api/APIServer.java
+++ src/main/java/ca/ulaval/gif3001/ima/api/APIServer.java
@@ -3,10 +3,6 @@ package ca.ulaval.gif3001.ima.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javaslang.control.Try;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import static spark.Spark.*;
 
 public class APIServer {
@@ -14,20 +10,17 @@ public class APIServer {
     private static ObjectMapper jsonObjectMapper = new ObjectMapper();
 
     public static void main(String[] args) {
-        Integer portNumber = Try.of(() -> Integer.valueOf(System.getenv("PORT"))).orElseGet((t) -> {
-            System.err.println("There was an error retrieving PORT env var using the default one (8080)");
-            return 8080;
-        });
 
-        String dbUser = Try.of(() -> String.valueOf(System.getenv("DB_USER"))).orElseGet((t) -> {
-            System.err.println("There was an error retrieving DB_USER env var using the default one (robert)");
-            return "robert";
-        });
+        InitEnvVars initEnvVars = new InitEnvVars().invoke();
+        Integer portNumber = initEnvVars.getPortNumber();
+        String dbUser = initEnvVars.getDbUser();
+        String dbPassword = initEnvVars.getDbPassword();
+        String dbHost = initEnvVars.getDbHost();
 
         port(portNumber);
 
         get("/", (req, res) -> "Project dashboard api");
-        get("/status", (req, res) -> String.format("Running on port %s, database user %s", portNumber, dbUser));
+        get("/status", (req, res) -> String.format("Running on port %s, database user %s, database password %s, database host %s", portNumber, dbUser, dbPassword, dbHost));
         get("/ping", (req, res) -> "pong bang crash");
 
         options("*", (request, response) -> "");
@@ -37,5 +30,60 @@ public class APIServer {
             response.header("Access-Control-Allow-Headers", "*");
             response.type("application/json");
         });
+    }
+
+    private static class InitEnvVars {
+        private Integer portNumber;
+        private String dbUser;
+        private String dbPassword;
+        private String dbHost;
+
+        public Integer getPortNumber() {
+            return portNumber;
+        }
+
+        public String getDbUser() {
+            return dbUser;
+        }
+
+        public String getDbPassword() {
+            return dbPassword;
+        }
+
+        public String getDbHost() {
+            return dbHost;
+        }
+
+        public InitEnvVars invoke() {
+            portNumber = Try.of(() -> Integer.valueOf(System.getenv("PORT"))).orElseGet((t) -> {
+                System.err.println("There was an error retrieving PORT env var using the default one (8080)");
+                return 8080;
+            });
+
+
+            dbUser = Try.of(() -> stringValueOf(System.getenv("DB_USER"))).orElseGet((t) -> {
+                System.err.println("There was an error retrieving DB_USER env var using the default one (root)");
+                return "robert";
+            });
+
+            dbPassword = Try.of(() -> stringValueOf(System.getenv("DB_PASSWORD"))).orElseGet((t) -> {
+                System.err.println("There was an error retrieving DB_PASSWORD env var using the default one (\"\")");
+                return "";
+            });
+
+            dbHost = dbPassword = Try.of(() -> stringValueOf(System.getenv("DB_HOST"))).orElseGet((t) -> {
+                System.err.println("There was an error retrieving DB_PASSWORD env var using the default one (localhost)");
+                return "localhost";
+            });
+
+            return this;
+        }
+
+        private String stringValueOf(String value) throws Exception {
+            if (value == null) {
+                throw new Exception("Invalid argument, cannot be of null value");
+            }
+            return String.valueOf(value);
+        }
     }
 }
